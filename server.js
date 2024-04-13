c54d3316945f66973f87ce4b529fa808c7143f01
AdamJamesWatkins <adamjwatkins@gmail.com>
2024-04-13 16:52:35 +0100

    Allow static files to be returned
    Added a call to app.use to check for the route '/static' and resolve the path to the static content.  This allows the javascript file referenced in index.html to be returned

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const port = 8083;
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 8083, () => {
-    console.log("Server is running on port 5060");
+app.listen(process.env.PORT || port, () => {
+    console.log("Server is running on port " + port);
 });
